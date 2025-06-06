b53b732460c78a03ecf2493d88d6b6924ffb2343
Apricot-S <mahjong_apricot@outlook.jp>
2025-06-07 00:05:18 +0900

    docs: Add doc comment

--- common/src/types.rs
+++ common/src/types.rs
@@ -1,14 +1,26 @@
 use crate::constants::TILE_TYPE_COUNT;
 
-/// 牌の枚数を表す数
+/// Count of tiles.
 pub type TileCount = u8;
 
-/// 34種類の牌の枚数を表す配列
+/// Array representing the number of tiles for each of the 34 tile types
 ///
-/// 牌の種類は、0から33までの整数で表される
-/// インデックスと牌の種類は以下のように対応する
-/// 0: 1m, 1: 2m, ..., 8: 9m,
-/// 9: 1p, ..., 17: 9p,
-/// 18: 1s, ..., 26: 9s,
-/// 27: 東, 28: 南, 29: 西, 30: 北, 31: 白, 32: 發, 33: 中
+/// Tile types are represented by integers from 0 to 33.
+/// The correspondence between indices and tile types is as follows:
+///
+/// | Index | 0   | 1   | 2   | 3   | 4   | 5   | 6   | 7   | 8   |
+/// | ----- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+/// | Tile  | 1m  | 2m  | 3m  | 4m  | 5m  | 6m  | 7m  | 8m  | 9m  |
+///
+/// | Index | 9   | 10  | 11  | 12  | 13  | 14  | 15  | 16  | 17  |
+/// | ----- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+/// | Tile  | 1p  | 2p  | 3p  | 4p  | 5p  | 6p  | 7p  | 8p  | 9p  |
+///
+/// | Index | 18  | 19  | 20  | 21  | 22  | 23  | 24  | 25  | 26  |
+/// | ----- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+/// | Tile  | 1s  | 2s  | 3s  | 4s  | 5s  | 6s  | 7s  | 8s  | 9s  |
+///
+/// | Index | 27        | 28         | 29        | 30         | 31         | 32         | 33       |
+/// | ----- | --------- | ---------- | --------- | ---------- | ---------- | ---------- | -------- |
+/// | Tile  | East (1z) | South (2z) | West (3z) | North (4z) | White (5z) | Green (6z) | Red (7z) |
 pub type TileCounts = [TileCount; TILE_TYPE_COUNT as usize];
