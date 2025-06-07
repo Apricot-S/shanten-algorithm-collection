48bba133a6be311e7b6791ccacdb01678c314f01
Apricot-S <mahjong_apricot@outlook.jp>
2025-06-08 02:28:48 +0900

    refactor: Improve macros

--- common/src/macros.rs
+++ common/src/macros.rs
@@ -13,8 +13,12 @@
 #[macro_export]
 macro_rules! shanten_test_case {
     ($calculator_type:ty, $hand:expr, $expected:expr) => {
-        let calculator = <$calculator_type>::new();
-        assert_eq!(calculator.calculate_shanten(&$hand), $expected);
+        let calculator = <$calculator_type as $crate::ShantenCalculator>::new();
+        let counts = TileCounts::from_code(&$hand);
+        assert_eq!(
+            $crate::ShantenCalculator::calculate_shanten(&calculator, &counts),
+            $expected
+        );
     };
 }
 
@@ -59,38 +63,22 @@ macro_rules! shanten_tests {
 
             #[test]
             fn test_shanten_thirteen_orphans() {
-                shanten_test_case!(
-                    $calculator_type,
-                    TileCounts::from_code("11m19p19s1234567z"),
-                    7
-                );
+                shanten_test_case!($calculator_type, "11m19p19s1234567z", 7);
             }
 
             #[test]
             fn test_shanten_thirteen_wait_thirteen_orphans() {
-                shanten_test_case!(
-                    $calculator_type,
-                    TileCounts::from_code("19m19p19s1234567z"),
-                    8
-                );
+                shanten_test_case!($calculator_type, "19m19p19s1234567z", 8);
             }
 
             #[test]
             fn test_shanten_tenpai() {
-                shanten_test_case!(
-                    $calculator_type,
-                    TileCounts::from_code("123m456p789s1122z"),
-                    0
-                );
+                shanten_test_case!($calculator_type, "123m456p789s1122z", 0);
             }
 
             #[test]
             fn test_shanten_win() {
-                shanten_test_case!(
-                    $calculator_type,
-                    TileCounts::from_code("123m456p789s11222z"),
-                    -1
-                );
+                shanten_test_case!($calculator_type, "123m456p789s11222z", -1);
             }
         }
     };
