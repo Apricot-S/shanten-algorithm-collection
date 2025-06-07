ff85e377f04c802d47f8c2f521a5b69301f5a789
Apricot-S <mahjong_apricot@outlook.jp>
2025-06-07 21:16:44 +0900

    docs: translate doc comments

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -1,8 +1,11 @@
-// 同一の牌の最大枚数
+/// Maximum number of identical tiles
 pub const MAX_TILE_COUNT: i8 = 4;
-// 牌の種類の数
+
+/// Number of tile types
 pub const TILE_TYPE_COUNT: i8 = 34;
-// 手牌の最小枚数
+
+/// Minimum number of tiles in a hand
 pub const MIN_HAND_SIZE: i8 = 1;
-// 手牌の最大枚数
+
+/// Maximum number of tiles in a hand
 pub const MAX_HAND_SIZE: i8 = 14;
