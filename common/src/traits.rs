a2667c53f720a0321c7562bc86dec642aedbaec1
Apricot-S <mahjong_apricot@outlook.jp>
2025-06-07 21:37:49 +0900

    docs: Add doc comment

--- common/src/traits.rs
+++ common/src/traits.rs
@@ -1,6 +1,22 @@
 use crate::types::TileCounts;
 
+/// Trait for implementing shanten number calculation algorithms.
+///
+/// Please implement this trait to add your own algorithm.
 pub trait ShantenCalculator {
+    /// Creates a new instance.
+    ///
+    /// If your algorithm requires loading tables or initialization,
+    /// please perform it here.
     fn new() -> Self;
+
+    /// Calculates the shanten number of a hand.
+    ///
+    /// # Arguments
+    /// * `hand` - Reference to a [TileCounts] struct representing the hand's tile counts
+    ///
+    /// # Returns
+    /// * Returns the shanten number as an integer.
+    /// * Return `0` for tenpai (ready hand), and `-1` for a winning hand.
     fn calculate_shanten(&self, hand: &TileCounts) -> i8;
 }
